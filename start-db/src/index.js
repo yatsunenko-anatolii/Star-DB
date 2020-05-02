7fc067f6f24318d8ad464c5bdc372f170418016b
waiter-development <yatsunenko90@gmail.com>
2020-05-02 14:39:03 +0300

    first-api

--- start-db/src/index.js
+++ start-db/src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class SwapiService {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+  _apiBase ='https://swapi.dev/api'
+
+  async getResource(url) {
+    
+    const res = await fetch(`${this._apiBase}${url}`)
+
+     if(!res.ok) {
+        throw new Error(`Could not fetch ${url}` + 
+        `, received ${res.status}`)
+      }
+
+  return await res.json()
+  }
+
+ async getAllPeople() {
+    const res = await this.getResource(`/people/`)
+    return res.results
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`)
+    return res.results
+  }
+  getPlanet(id) {
+    return this.getResource(`/plantes/${id}`)
+  }
+  
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`)
+    return res.results
+  }
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`)
+  }
+}
+
+
+const swapi = new SwapiService()
+swapi.getAllPeople()
+        .then((people) => {
+          people.forEach((p) => {
+            console.log(p.eye_color)
+          })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+swapi.getPerson(4)
+        .then((p)=> {
+          console.log(p.name)
+        })
+swapi.getAllPlanets()
+        .then((body) => {
+          console.log(body)
+        })
+
+        
+
+
+
+
+
+
+
+
+
+
