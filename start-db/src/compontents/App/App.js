9d2e9d24f86cbd122fcb655b9e3b7062908e42ca
waiter-developer <yatsunenko90@gmail.com>
2020-05-20 15:11:04 +0300

    created-API_components

--- start-db/src/compontents/App/App.js
+++ start-db/src/compontents/App/App.js
@@ -12,7 +12,6 @@ import './App.css'
 export default class App extends Component {
 
 
-
     render() {
 
     return (
--- start-db/src/compontents/Random_Planet/Random_Planet.js
+++ start-db/src/compontents/Random_Planet/Random_Planet.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react'
 
-
+import SwapiService from  '../../services/swapi_service'
 import './Random_Planet.css'
 
 
 export default class RandomPlanet extends Component {
 
+  swapiService = new SwapiService()
+
+
+  state = {
+    planet: {}
+  }
+
+  constructor() {
+    super();
+    this.updatePlanet()
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({planet})
+  }
+  updatePlanet() {
+    const id = Math.floor(Math.random()*25) + 2
+    this.swapiService.getPlanet(id)
+        .then(this.onPlanetLoaded)
+
+  }
+
 
   render() {
-    
+
+    const { planet: {id, planetName,population,
+            rotationPeriod, diameter} } = this.state
+
+
     return ( 
       <div className="Random_planet d-flex">
-        <img src={`https://images.unsplash.com/photo-1533294455009-a77b7557d2d1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80`} className="Random_planer_img" alt="Air Spaxe" title="AirSpace"></img>
+        <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} className="Random_planer_img" alt="Air Spaxe" title="AirSpace"></img>
         
         <div className="Random_planet_list">
-          <h4>Planet Name</h4>
+          <h4>{planetName}</h4>
           <ul>
-            <li>
-              Population: <span>123124</span>
+            <li className="planet_list--item">
+              <span className="term">Population: </span>
+              <span>{population}</span>
             </li>
-            <li>
-              Rotation Period: <span>34</span>
+            <li className="planet_list--item">
+              <span className="term">Rotation Period: </span>
+              <span>{rotationPeriod}</span>
             </li>
-            <li>
-              Diameter: <span>123124</span>
+            <li className="planet_list--item">
+              <span className="term">Diameter: </span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
